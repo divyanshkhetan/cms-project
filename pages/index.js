94b1e50140d3b296cd934c1b9ccd02a6ed6309cd
divyanshkhetan <divyanshkhetan18@gmail.com>
2022-05-06 21:17:22 +0530

    indexpage skeleton ready

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -11,44 +11,64 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main className={styles.container}>
+        <div className={styles.headerContainer}>
+          <div className="banner">CMS</div>
+          <div className={styles.loginSection}>
+            <button className={styles.signup}>Sign Up</button>
+            <button className={styles.login}>Login</button>
+          </div>
+        </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <div className={styles.contentContainer}>
+          <div className={styles.vectorContainer}>
+            <div className={styles.vectorText}>
+              <div>
+                <div className={styles.lsnText}>Learn</div>
+                <div className={styles.lsnText}>Something</div>
+                <div className={styles.lsnText}>New</div>
+              </div>
+            </div>
+            <div className={styles.vector}>
+              <Image
+                alt="online education"
+                src="/images/pablo-book-lover.png"
+                width={800}
+                height={600}
+              />
+            </div>
+          </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={styles.featureList}>
+            <div className={styles.feature}>
+              <Image src="/images/icons8-quiz-100.png" width={75} height={75} />
+              <div className={styles.featureText}>
+                {" "}
+                <div>Automated</div>
+                <div>Assessment </div>{" "}
+              </div>
+            </div>
+            <div className={styles.feature}>
+              <Image
+                src="/images/icons8-coaching-96.png"
+                width={75}
+                height={75}
+              />
+              <div className={styles.featureText}>
+                {" "}
+                <div>Mentor</div>
+                <div>Support </div>{" "}
+              </div>
+            </div>
+            <div className={styles.feature}>
+              <Image src="/images/icons8-chat-96.png" width={75} height={75} />
+              <div className={styles.featureText}>
+                {" "}
+                <div>Live</div>
+                <div>Chatting </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
 
@@ -58,12 +78,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
